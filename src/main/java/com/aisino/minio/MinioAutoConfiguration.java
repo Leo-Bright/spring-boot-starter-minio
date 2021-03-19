3c2eb04bfabbdf9057100874e29688571bb3befa
Leo Bright <laixinming@aisino.com>
2021-03-19 08:50:47 +0800

    init

---
+++ src/main/java/com/aisino/minio/MinioAutoConfiguration.java
@@ -0,0 +1,29 @@
+package com.aisino.minio;
+
+import com.aisino.minio.service.MinioTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({ MinioProperties.class })
+public class MinioAutoConfiguration {
+
+    @Autowired
+    private MinioProperties properties;
+
+    @Bean
+    @ConditionalOnMissingBean(MinioTemplate.class)
+    @ConditionalOnProperty(name = "minio.url")
+    MinioTemplate template(){
+        return new MinioTemplate(
+                properties.getUrl(),
+                properties.getAccessKey(),
+                properties.getSecretKey()
+        );
+    }
+
+
+}
